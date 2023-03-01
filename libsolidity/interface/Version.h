fb85eaf69a01a29d8fa31f232c0b8c5d00aa7b66
Abinav Puthan Purayil <abinavpp@gmail.com>
2023-03-01 11:42:25 +0530

    Add zkevm specific details in the --version output
    This patch also adds the SOLC_VERSION_ZKEVM cmake setting to configure
    the zkevm version.

--- libsolidity/interface/Version.h
+++ libsolidity/interface/Version.h
@@ -34,5 +34,6 @@ extern std::string const VersionString;
 extern std::string const VersionStringStrict;
 extern bytes const VersionCompactBytes;
 extern bool const VersionIsRelease;
+extern char const* ZKEVMVersionString;
 
 }
