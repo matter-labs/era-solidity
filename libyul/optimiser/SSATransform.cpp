ethereum/solidity

fix warning in GCC 9.1: redundant move in return statement

libyul/optimiser/SSATransform.cpp
@@ -106,7 +106,7 @@ void SSATransform::operator()(Block& _block)
 					});
 				}
 				boost::get<VariableDeclaration>(statements.front()).variables = std::move(newVariables);
-				return std::move(statements);
+				return { std::move(statements) };
 			}
 			else if (_s.type() == typeid(Assignment))
 			{
@@ -133,7 +133,7 @@ void SSATransform::operator()(Block& _block)
 					});
 				}
 				boost::get<VariableDeclaration>(statements.front()).variables = std::move(newVariables);
-				return std::move(statements);
+				return { std::move(statements) };
 			}
 			else
 				visit(_s);

matter-labs/era-solidity

libyul/optimiser/SSATransform.cpp
@@ -97,7 +97,7 @@ void SSATransform::operator()(Block& _block)
 					varDecl.value
 				));
 				v.emplace_back(move(varDecl));
-				return std::move(v);
+				return { std::move(v) };
 			}
 			else if (_s.type() == typeid(Assignment))
 			{
@@ -115,7 +115,7 @@ void SSATransform::operator()(Block& _block)
 					assignment.value
 				));
 				v.emplace_back(move(assignment));
-				return std::move(v);
+				return { std::move(v) };
 			}
 			else
 				visit(_s);