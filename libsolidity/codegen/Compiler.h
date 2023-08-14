24385e8e645abea0967844c6f5e894cd7f51ea5b
Abinav Puthan Purayil <abinavpp@gmail.com>
2023-08-14 13:01:57 +0000

    Implement the workaround for zksolc to lower recursion; Lower indirect calls using selectors
    * Use a selector based lowering for internal indirect function call lowering in the legacy pipeline
    
      This change lowers internal indirect calls as switch statements
      involving the possible tags for dispatch. The lowering resembles to
      the yul based lowering of indirect calls.
    
    * Add FuncPtrTracker to minimize the selector
    
    * Try to infer direct calls even if ast says indirect
    
    * Implement the json metadata for tracking recursive functions
    
      This change gets the --standard-json compilation output to store
      information of functions in recursion under the "extraMetadata" field
      in the json output. This information is required by zksolc to lower
      functions in recursion correctly
    
    * Disable libevmasm's Inliner, JumpdestRemover and BlockDeduplicator
    
      This change is to avoid the optimiser to potentially invalidate the
      recursion metadata
    
    * Implement CycleFinder for solidity function ast nodes that also checks
      for potential cycles due to indirect calls
    
    * Integrate CycleFinder in the metadata printer
    
    * Use libyul's CallGraphGenerator to find cycles in inline assembly
    
    * Report low level utility functions for recursive structs as recursive
    
    * Use the Disambiguator on the inline-asm if optimizations are disabled;
      Get the Disambiguator and the AsmAnalyzer to work with inline-asm having external references
    
      The Disambiguator is scheduled in the optimization pipeline. We need
      it without the optimizations for the call-graph analysis to work with
      functions in different scopes having the same name.
    
    * Force disable libevmasm's Assembly::optimise()
    
      Decided to do this after noticing the changes in the handling of
      optimizer.settings in older releases

--- libsolidity/codegen/Compiler.h
+++ libsolidity/codegen/Compiler.h
@@ -60,12 +60,14 @@ public:
 
 	std::string generatedYulUtilityCode() const { return m_context.generatedYulUtilityCode(); }
 	std::string runtimeGeneratedYulUtilityCode() const { return m_runtimeContext.generatedYulUtilityCode(); }
+	Json::Value extraMetadata() const { return m_extraMetadata; }
 
 	/// @returns the entry label of the given function. Might return an AssemblyItem of type
 	/// UndefinedItem if it does not exist yet.
 	evmasm::AssemblyItem functionEntryLabel(FunctionDefinition const& _function) const;
 
 private:
+	Json::Value m_extraMetadata;
 	OptimiserSettings const m_optimiserSettings;
 	CompilerContext m_runtimeContext;
 	size_t m_runtimeSub = size_t(-1); ///< Identifier of the runtime sub-assembly, if present.
