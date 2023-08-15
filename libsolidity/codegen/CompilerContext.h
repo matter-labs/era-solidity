cc763d9fa5695f9333bad0d62fecb3c27e4b390e
Abinav Puthan Purayil <abinavpp@gmail.com>
2023-08-15 08:29:14 +0000

    Implement the workaround for zksolc to lower recursion; Lower indirect calls using selectors
    * Use a selector based lowering for internal indirect function call lowering in the legacy pipeline
    
      This change lowers internal indirect calls as switch statements
      involving the possible tags for dispatch. The lowering resembles to
      the yul based lowering of indirect calls.
    
    * Add FuncPtrTracker to minimize the selector
    
    * Try to infer direct calls even if ast says indirect
    
    * Implement the json metadata for tracking recursive functions
    
      This change gets the --standard-json compilation output to store
      information of functions in recursion under the "extraMetadata" field
      in the json output. This information is required by zksolc to lower
      functions in recursion correctly
    
    * Disable libevmasm's Inliner, JumpdestRemover and BlockDeduplicator
    
      This change is to avoid the optimiser to potentially invalidate the
      recursion metadata
    
    * Implement CycleFinder for solidity function ast nodes that also checks
      for potential cycles due to indirect calls
    
    * Integrate CycleFinder in the metadata printer
    
    * Use libyul's CallGraphGenerator to find cycles in inline assembly
    
    * Report low level utility functions for recursive structs as recursive
    
    * Use the Disambiguator on the inline-asm if optimizations are disabled;
      Get the Disambiguator and the AsmAnalyzer to work with inline-asm having external references
    
      The Disambiguator is scheduled in the optimization pipeline. We need
      it without the optimizations for the call-graph analysis to work with
      functions in different scopes having the same name.
    
    * Force disable libevmasm's Assembly::optimise()
    
      Decided to do this after noticing the changes in the handling of
      optimizer.settings in older releases

--- libsolidity/codegen/CompilerContext.h
+++ libsolidity/codegen/CompilerContext.h
@@ -38,6 +38,7 @@
 
 #include <libyul/AsmAnalysisInfo.h>
 #include <libyul/backends/evm/EVMDialect.h>
+#include <libyul/backends/evm/EVMCodeTransform.h>
 
 #include <functional>
 #include <ostream>
@@ -112,6 +113,9 @@ public:
 	FunctionDefinition const* nextConstructor(ContractDefinition const& _contract) const;
 	/// Sets the current inheritance hierarchy from derived to base.
 	void setInheritanceHierarchy(std::vector<ContractDefinition const*> const& _hierarchy) { m_inheritanceHierarchy = _hierarchy; }
+    /// Sets the contract currently being compiled - the most derived one.
+    void setMostDerivedContract(ContractDefinition const& _contract) { m_mostDerivedContract = &_contract; }
+    ContractDefinition const& mostDerivedContract() const;
 
 	/// @returns the next function in the queue of functions that are still to be compiled
 	/// (i.e. that were referenced during compilation but where we did not yet generate code for).
@@ -150,6 +154,10 @@ public:
 		unsigned _outArgs,
 		std::function<void(CompilerContext&)> const& _generator
 	);
+	/// Returns the entry tag of the low-level function with the name @a _name if already generated; Returns
+	/// evmasm::AssemblyItem(evmasm::UndefinedItem) if the entry tag is not generated.
+	evmasm::AssemblyItem lowLevelFunctionTagIfExists(std::string const& _name);
+
 	/// Generates the code for missing low-level functions, i.e. calls the generators passed above.
 	void appendMissingLowLevelFunctions();
 	ABIFunctions& abiFunctions() { return m_abiFunctions; }
@@ -285,6 +293,40 @@ public:
 	evmasm::LinkerObject const& assembledObject() const { return m_asm->assemble(); }
 	evmasm::LinkerObject const& assembledRuntimeObject(size_t _subIndex) const { return m_asm->sub(_subIndex).assemble(); }
 
+	/// Adds the @a _asm -> @a _context mapping in the internal inline assembly to context mapping
+	void addInlineAsmContextMapping(InlineAssembly const* _asm, std::shared_ptr<yul::CodeTransformContext> _context)
+	{
+		m_inlineAsmContextMap[_asm] = _context;
+	}
+
+	/// Returns the context for @a _asm; nullptr if not found
+	yul::CodeTransformContext const* findInlineAsmContextMapping(InlineAssembly const* _asm) const
+	{
+		auto findIt = m_inlineAsmContextMap.find(_asm);
+		if (findIt == m_inlineAsmContextMap.end())
+			return nullptr;
+		return findIt->second.get();
+	}
+
+	struct FunctionInfo
+	{
+		std::string const name;
+		unsigned tag;
+		unsigned ins;
+		unsigned outs;
+
+		bool operator<(FunctionInfo const& _other) const
+		{
+			return tie(name, tag, ins, outs) < tie(_other.name, _other.tag, _other.ins, _other.outs);
+		}
+	};
+
+	/// Adds @a _func to the set of low level utility functions that are recursive
+	void addRecursiveLowLevelFunc(FunctionInfo _func) { m_recursiveLowLevelFuncs.insert(_func); }
+
+	/// Returns the set of low level utility functions that are recursive
+	std::set<FunctionInfo> const& recursiveLowLevelFuncs() const { return m_recursiveLowLevelFuncs; }
+
 	/**
 	 * Helper class to pop the visited nodes stack when a scope closes
 	 */
@@ -367,6 +409,8 @@ private:
 	std::map<Declaration const*, std::vector<unsigned>> m_localVariables;
 	/// List of current inheritance hierarchy from derived to base.
 	std::vector<ContractDefinition const*> m_inheritanceHierarchy;
+    /// The contract currently being compiled. Virtual function lookup starts from this contarct.
+    ContractDefinition const* m_mostDerivedContract = nullptr;
 	/// Stack of current visited AST nodes, used for location attachment
 	std::stack<ASTNode const*> m_visitedNodes;
 	/// The runtime context if in Creation mode, this is used for generating tags that would be stored into the storage and then used at runtime.
@@ -385,6 +429,10 @@ private:
 	YulUtilFunctions m_yulUtilFunctions;
 	/// The queue of low-level functions to generate.
 	std::queue<std::tuple<std::string, unsigned, unsigned, std::function<void(CompilerContext&)>>> m_lowLevelFunctionGenerationQueue;
+	/// Maps an InlineAssembly AST node to its CodeTransformContext created during its lowering
+	std::map<InlineAssembly const*, std::shared_ptr<yul::CodeTransformContext>> m_inlineAsmContextMap;
+	/// Set of low level utility functions generated in this context that are recursive
+	std::set<FunctionInfo> m_recursiveLowLevelFuncs;
 };
 
 }
