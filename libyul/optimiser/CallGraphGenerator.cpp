1f792eaf0275995dfba87b39ec67719a85c20701
Abinav Puthan Purayil <abinavpp@gmail.com>
2023-09-21 10:54:05 +0000

    Implement the workaround for zksolc to lower recursion; Lower indirect calls using selectors
    * Use a selector based lowering for internal indirect function call lowering in the legacy pipeline
    
      This change lowers internal indirect calls as switch statements
      involving the possible tags for dispatch. The lowering resembles to
      the yul based lowering of indirect calls.
    
    * Add FuncPtrTracker to minimize the selector
    
    * Try to infer direct calls even if ast says indirect
    
    * Implement the json metadata for tracking recursive functions
    
      This change gets the --standard-json compilation output to store
      information of functions in recursion under the "extraMetadata" field
      in the json output. This information is required by zksolc to lower
      functions in recursion correctly
    
    * Disable libevmasm's Inliner, JumpdestRemover and BlockDeduplicator
    
      This change is to avoid the optimiser to potentially invalidate the
      recursion metadata
    
    * Implement CycleFinder for solidity function ast nodes that also checks
      for potential cycles due to indirect calls
    
    * Integrate CycleFinder in the metadata printer
    
    * Use libyul's CallGraphGenerator to find cycles in inline assembly
    
    * Report low level utility functions for recursive structs as recursive
    
    * Use the Disambiguator on the inline-asm if optimizations are disabled;
      Get the Disambiguator and the AsmAnalyzer to work with inline-asm having external references
    
      The Disambiguator is scheduled in the optimization pipeline. We need
      it without the optimizations for the call-graph analysis to work with
      functions in different scopes having the same name.
    
    * Force disable libevmasm's Assembly::optimise()
    
      Decided to do this after noticing the changes in the handling of
      optimizer.settings in older releases

--- libyul/optimiser/CallGraphGenerator.cpp
+++ libyul/optimiser/CallGraphGenerator.cpp
@@ -29,6 +29,47 @@ using namespace std;
 using namespace dev;
 using namespace yul;
 
+namespace
+{
+// TODO: This algorithm is non-optimal.
+struct CallGraphCycleFinder
+{
+	CallGraph const& callGraph;
+	set<YulString> containedInCycle{};
+	set<YulString> visited{};
+	vector<YulString> currentPath{};
+
+	void visit(YulString _function)
+	{
+		if (visited.count(_function))
+			return;
+		if (
+			auto it = find(currentPath.begin(), currentPath.end(), _function);
+			it != currentPath.end()
+		)
+			containedInCycle.insert(it, currentPath.end());
+		else
+		{
+			currentPath.emplace_back(_function);
+			if (callGraph.functionCalls.count(_function))
+				for (auto const& child: callGraph.functionCalls.at(_function))
+					visit(child);
+			currentPath.pop_back();
+			visited.insert(_function);
+		}
+	}
+};
+}
+
+set<YulString> CallGraph::recursiveFunctions() const
+{
+	CallGraphCycleFinder cycleFinder{*this};
+	// Visiting the root only is not enough, since there may be disconnected recursive functions.
+	for (auto const& call: functionCalls)
+		cycleFinder.visit(call.first);
+	return cycleFinder.containedInCycle;
+}
+
 map<YulString, set<YulString>> CallGraphGenerator::callGraph(Block const& _ast)
 {
 	CallGraphGenerator gen;
@@ -36,18 +77,32 @@ map<YulString, set<YulString>> CallGraphGenerator::callGraph(Block const& _ast)
 	return std::move(gen.m_callGraph);
 }
 
+CallGraph CallGraphGenerator::callGraph2(Block const& _ast)
+{
+	CallGraphGenerator gen;
+	gen(_ast);
+	return std::move(gen.m_callGraph2);
+}
+
 void CallGraphGenerator::operator()(FunctionalInstruction const& _functionalInstruction)
 {
 	string name = dev::eth::instructionInfo(_functionalInstruction.instruction).name;
 	std::transform(name.begin(), name.end(), name.begin(), [](unsigned char _c) { return tolower(_c); });
 	m_callGraph[m_currentFunction].insert(YulString{name});
+	m_callGraph2.functionCalls[m_currentFunction].insert(YulString{name});
 	ASTWalker::operator()(_functionalInstruction);
 }
 
 void CallGraphGenerator::operator()(FunctionCall const& _functionCall)
 {
 	m_callGraph[m_currentFunction].insert(_functionCall.functionName.name);
+	m_callGraph2.functionCalls[m_currentFunction].insert(_functionCall.functionName.name);
 	ASTWalker::operator()(_functionCall);
+}
+
+void CallGraphGenerator::operator()(ForLoop const&)
+{
+	m_callGraph2.functionsWithLoops.insert(m_currentFunction);
 }
 
 void CallGraphGenerator::operator()(FunctionDefinition const& _functionDefinition)
@@ -55,7 +110,9 @@ void CallGraphGenerator::operator()(FunctionDefinition const& _functionDefinitio
 	YulString previousFunction = m_currentFunction;
 	m_currentFunction = _functionDefinition.name;
 	yulAssert(m_callGraph.count(m_currentFunction) == 0, "");
+	yulAssert(m_callGraph2.functionCalls.count(m_currentFunction) == 0, "");
 	m_callGraph[m_currentFunction] = {};
+	m_callGraph2.functionCalls[m_currentFunction] = {};
 	ASTWalker::operator()(_functionDefinition);
 	m_currentFunction = previousFunction;
 }
@@ -63,5 +120,6 @@ void CallGraphGenerator::operator()(FunctionDefinition const& _functionDefinitio
 CallGraphGenerator::CallGraphGenerator()
 {
 	m_callGraph[YulString{}] = {};
+	m_callGraph2.functionCalls[YulString{}] = {};
 }
 
