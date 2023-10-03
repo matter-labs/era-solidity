66eb0ffd6420ff72e59b628466130e3bbdae7d8b
Abinav Puthan Purayil <abinavpp@gmail.com>
2023-10-03 15:30:52 +0200

    Implement the workaround for zksolc to lower recursion; Lower indirect calls using selectors
    * Use a selector based lowering for internal indirect function call lowering in the legacy pipeline
    
      This change lowers internal indirect calls as switch statements
      involving the possible tags for dispatch. The lowering resembles to
      the yul based lowering of indirect calls.
    
    * Add FuncPtrTracker to minimize the selector
    
    * Try to infer direct calls even if ast says indirect
    
    * Implement the json metadata for tracking recursive functions
    
      This change gets the --standard-json compilation output to store
      information of functions in recursion under the "extraMetadata" field
      in the json output. This information is required by zksolc to lower
      functions in recursion correctly
    
    * Disable libevmasm's Inliner, JumpdestRemover and BlockDeduplicator
    
      This change is to avoid the optimiser to potentially invalidate the
      recursion metadata
    
    * Implement CycleFinder for solidity function ast nodes that also checks
      for potential cycles due to indirect calls
    
    * Integrate CycleFinder in the metadata printer
    
    * Use libyul's CallGraphGenerator to find cycles in inline assembly
    
    * Report low level utility functions for recursive structs as recursive
    
    * Use the Disambiguator on the inline-asm if optimizations are disabled;
      Get the Disambiguator and the AsmAnalyzer to work with inline-asm having external references
    
      The Disambiguator is scheduled in the optimization pipeline. We need
      it without the optimizations for the call-graph analysis to work with
      functions in different scopes having the same name.
    
    * Force disable libevmasm's Assembly::optimise()
    
      Decided to do this after noticing the changes in the handling of
      optimizer.settings in older releases

--- libsolidity/codegen/ArrayUtils.cpp
+++ libsolidity/codegen/ArrayUtils.cpp
@@ -296,6 +296,17 @@ void ArrayUtils::copyArrayToStorage(ArrayType const& _targetType, ArrayType cons
 			_context << Instruction::POP;
 		}
 	);
+
+	if (auto* structType = dynamic_cast<StructType const*>(_sourceType.baseType()))
+	{
+		if (structType->recursive())
+		{
+      std::string name{"$copyArrayToStorage_" + sourceType->identifier() + "_to_" + targetType->identifier()};
+			auto tag = m_context.lowLevelFunctionTagIfExists(name);
+			solAssert(tag != evmasm::AssemblyItem(evmasm::UndefinedItem), "");
+			m_context.addRecursiveLowLevelFunc({name, tag.data().convert_to<uint32_t>(), /*ins=*/3, /*outs=*/1});
+		}
+	}
 }
 
 void ArrayUtils::copyArrayToMemory(ArrayType const& _sourceType, bool _padToWordBoundaries) const
@@ -598,6 +609,17 @@ void ArrayUtils::clearArray(ArrayType const& _typeIn) const
 			solAssert(_context.stackHeight() == stackHeightStart - 2, "");
 		}
 	);
+
+	if (auto* structType = dynamic_cast<StructType const*>(_typeIn.baseType()))
+	{
+		if (structType->recursive())
+		{
+      std::string name{"$clearArray_" + _typeIn.identifier()};
+			auto tag = m_context.lowLevelFunctionTagIfExists(name);
+			solAssert(tag != evmasm::AssemblyItem(evmasm::UndefinedItem), "");
+			m_context.addRecursiveLowLevelFunc({name, tag.data().convert_to<uint32_t>(), /*ins=*/2, /*outs=*/0});
+		}
+	}
 }
 
 void ArrayUtils::clearDynamicArray(ArrayType const& _type) const
--- libsolidity/interface/CompilerStack.cpp
+++ libsolidity/interface/CompilerStack.cpp
@@ -48,6 +48,7 @@
 #include <libsolidity/ast/TypeProvider.h>
 #include <libsolidity/ast/ASTJsonImporter.h>
 #include <libsolidity/codegen/Compiler.h>
+#include <libsolidity/codegen/FuncPtrTracker.h>
 #include <libsolidity/formal/ModelChecker.h>
 #include <libsolidity/interface/ABI.h>
 #include <libsolidity/interface/Natspec.h>
@@ -119,6 +120,21 @@ CompilerStack::~CompilerStack()
 {
 	--g_compilerStackCounts;
 	TypeProvider::reset();
+}
+
+void CompilerStack::populateFuncPtrRefs()
+{
+	for (Source const* source: m_sourceOrder)
+	{
+		if (!source->ast)
+			continue;
+
+		for (ContractDefinition const* contract: ASTNode::filteredNodes<ContractDefinition>(source->ast->nodes()))
+		{
+			FuncPtrTracker tracker{*contract};
+			tracker.run();
+		}
+	}
 }
 
 void CompilerStack::createAndAssignCallGraphs()
@@ -599,6 +615,8 @@ bool CompilerStack::analyzeLegacy(bool _noErrorsSoFar)
 		{
 			ControlFlowRevertPruner pruner(cfg);
 			pruner.run();
+			populateFuncPtrRefs();
+			createAndAssignCallGraphs();
 
 			ControlFlowAnalyzer controlFlowAnalyzer(cfg, m_errorReporter);
 			if (!controlFlowAnalyzer.run())
@@ -1126,6 +1144,17 @@ std::string const& CompilerStack::metadata(Contract const& _contract) const
 	return _contract.metadata.init([&]{ return createMetadata(_contract, m_viaIR); });
 }
 
+Json::Value const& CompilerStack::extraMetadata(std::string const& _contractName) const
+{
+	Contract const& contr = contract(_contractName);
+	if (m_stackState < AnalysisSuccessful)
+		solThrow(CompilerError, "Analysis was not successful.");
+
+	solAssert(contr.contract, "");
+
+	return contr.extraMetadata;
+}
+
 CharStream const& CompilerStack::charStream(std::string const& _sourceName) const
 {
 	if (m_stackState < SourcesSet)
@@ -1456,6 +1485,7 @@ void CompilerStack::compileContract(
 		solAssert(false, "Optimizer exception during compilation");
 	}
 
+	compiledContract.extraMetadata = compiler->extraMetadata();
 	_otherCompilers[compiledContract.contract] = compiler;
 
 	assembleYul(_contract, compiler->assemblyPtr(), compiler->runtimeAssemblyPtr());
