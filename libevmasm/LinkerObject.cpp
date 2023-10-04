64697510f4ed5949527175735eb9890e43bf0b40
Abinav Puthan Purayil <abinavpp@gmail.com>
2023-10-04 15:29:46 +0200

    Replace the bytecode output with an error string

--- libevmasm/LinkerObject.cpp
+++ libevmasm/LinkerObject.cpp
@@ -48,6 +48,7 @@ void LinkerObject::link(map<string, h160> const& _libraryAddresses)
 
 string LinkerObject::toHex() const
 {
+	return "The EVM bytecode is unavailable in the zkEVM edition of solc";
 	string hex = solidity::util::toHex(bytecode);
 	for (auto const& ref: linkReferences)
 	{
