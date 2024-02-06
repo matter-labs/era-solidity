9c746759dd46d20503229a62b7a9bc30688ea52f
Abinav Puthan Purayil <abinavpp@gmail.com>
2024-02-06 14:40:45 +0100

    mlir: Strip no-debuginfo testing

--- test/lit/mlirCodegen/empty-function.sol
+++ test/lit/mlirCodegen/empty-function.sol
@@ -1,5 +1,4 @@
-// RUN: solc --mlir-action=print-init %s | FileCheck %s
-// RUN: solc --mlir-action=print-init --mmlir --mlir-print-debuginfo %s | FileCheck --check-prefix=DBG %s
+// RUN: solc --mlir-action=print-init --mmlir --mlir-print-debuginfo %s | FileCheck %s
 
 contract C {
   function f() public pure {}
@@ -9,19 +8,11 @@ contract C {
 // CHECK: module {
 // CHECK-NEXT:   sol.contract @C_5 {
 // CHECK-NEXT:     func.func @f_4() attributes {llvm.linkage = #llvm.linkage<private>} {
-// CHECK-NEXT:       return
-// CHECK-NEXT:     }
-// CHECK-NEXT:   } {interface_fns = [{selector = "26121ff0", sym = @f_4, type = () -> ()}], kind = #sol<ContractKind Contract>}
-// CHECK-NEXT: }
+// CHECK-NEXT:       return loc(#loc2)
+// CHECK-NEXT:     } loc(#loc2)
+// CHECK-NEXT:   } {interface_fns = [{selector = "26121ff0", sym = @f_4, type = () -> ()}], kind = #sol<ContractKind Contract>} loc(#loc1)
+// CHECK-NEXT: } loc(#loc0)
+// CHECK-NEXT: #loc0 = loc(unknown)
+// CHECK-NEXT: #loc1 = loc({{.*}}:2:0)
+// CHECK-NEXT: #loc2 = loc({{.*}}:3:2)
 // CHECK-EMPTY:
-// DBG: module {
-// DBG-NEXT:   sol.contract @C_5 {
-// DBG-NEXT:     func.func @f_4() attributes {llvm.linkage = #llvm.linkage<private>} {
-// DBG-NEXT:       return loc(#loc2)
-// DBG-NEXT:     } loc(#loc2)
-// DBG-NEXT:   } {interface_fns = [{selector = "26121ff0", sym = @f_4, type = () -> ()}], kind = #sol<ContractKind Contract>} loc(#loc1)
-// DBG-NEXT: } loc(#loc0)
-// DBG-NEXT: #loc0 = loc(unknown)
-// DBG-NEXT: #loc1 = loc({{.*}}:3:0)
-// DBG-NEXT: #loc2 = loc({{.*}}:4:2)
-// DBG-EMPTY:
