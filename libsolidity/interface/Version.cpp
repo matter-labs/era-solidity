e58154ab8b4fcb3256383d9af4e5290cc6105918
Anton Baliasnikov <aba@matterlabs.dev>
2024-05-28 17:36:16 +0100

    ci: optimize and unify scripts and build process

--- libsolidity/interface/Version.cpp
+++ libsolidity/interface/Version.cpp
@@ -26,7 +26,7 @@
 #include <solidity/BuildInfo.h>
 
 char const* solidity::frontend::VersionNumber = ETH_PROJECT_VERSION;
-char const* solidity::frontend::ZKsyncVersionString = SOLC_VERSION_ZKSYNC;
+char const* solidity::frontend::ZKsyncVersionString = SOL_VERSION_ZKSYNC;
 
 std::string const solidity::frontend::VersionString =
 	std::string(solidity::frontend::VersionNumber) +
