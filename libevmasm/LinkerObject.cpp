6af5a28167d0b8dbf919f2ef22c536a976b6073f
Anton Baliasnikov <aba@matterlabs.dev>
2024-06-03 10:50:44 +0100

    ci: optimize and unify scripts and build process

--- libevmasm/LinkerObject.cpp
+++ libevmasm/LinkerObject.cpp
@@ -46,7 +46,7 @@ void LinkerObject::link(map<string, h160> const& _libraryAddresses)
 
 string LinkerObject::toHex() const
 {
-	return "The EVM bytecode is unavailable in the zkEVM edition of solc";
+	return "The EVM bytecode is unavailable in the ZKsync edition of solc";
 	string hex = dev::toHex(bytecode);
 	for (auto const& ref: linkReferences)
 	{
