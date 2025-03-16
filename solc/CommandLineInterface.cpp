03c333cb09babfdc392164df9fffd6693f84d5e3
Abinav Puthan Purayil <abinavpp@gmail.com>
2025-03-16 09:00:35 +0100

    Change the version dump

--- solc/CommandLineInterface.cpp
+++ solc/CommandLineInterface.cpp
@@ -823,9 +823,9 @@ void CommandLineInterface::processInput()
 
 void CommandLineInterface::printVersion()
 {
-	sout() << "solc, the ZKsync Solidity compiler commandline interface" << std::endl;
+	sout() << "solc, the LLVM Solidity compiler commandline interface" << std::endl;
 	sout() << "Version: " << solidity::frontend::VersionString << std::endl;
-	sout() << "ZKsync: " << solidity::frontend::ZKsyncVersionString << std::endl;
+	sout() << "LLVM: " << solidity::frontend::ZKsyncVersionString << std::endl;
 }
 
 void CommandLineInterface::printLicense()
