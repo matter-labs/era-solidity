d791d2f5aa7025ca9e9571c6bf6a9fa34fc65ecc
Abinav Puthan Purayil <abinavpp@gmail.com>
2025-04-24 12:54:43 +0200

    Track libraries in the FuncPtrTracker and the recursive function analysis

--- libsolidity/interface/CompilerStack.cpp
+++ libsolidity/interface/CompilerStack.cpp
@@ -230,11 +230,6 @@ bool CompilerStack::analyze()
 
 	if (noErrors)
 	{
-		populateFuncPtrRefs();
-	}
-
-	if (noErrors)
-	{
 		for (Source const* source: m_sourceOrder)
 			if (source->ast)
 				for (ASTPointer<ASTNode> const& node: source->ast->nodes())
@@ -254,6 +249,7 @@ bool CompilerStack::analyze()
 							)
 						);
 					}
+		populateFuncPtrRefs();
 	}
 
 	if (noErrors)
