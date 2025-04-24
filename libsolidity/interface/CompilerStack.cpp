ee9a2999b4daf80813f5e155e7c20795d360b06f
Abinav Puthan Purayil <abinavpp@gmail.com>
2025-04-24 12:54:43 +0200

    Track libraries in the FuncPtrTracker and the recursive function analysis

--- libsolidity/interface/CompilerStack.cpp
+++ libsolidity/interface/CompilerStack.cpp
@@ -502,8 +502,8 @@ bool CompilerStack::analyze()
 		// Create & assign callgraphs and check for contract dependency cycles
 		if (noErrors)
 		{
-			populateFuncPtrRefs();
 			createAndAssignCallGraphs();
+			populateFuncPtrRefs();
 			findAndReportCyclicContractDependencies();
 		}
 
