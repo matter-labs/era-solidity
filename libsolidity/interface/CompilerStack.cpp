2a0e9c40bda1abadb7e7f40c9ea1742853e0c708
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2025-07-05 16:27:56 +0800

    Check for the environment variable

--- libsolidity/interface/CompilerStack.cpp
+++ libsolidity/interface/CompilerStack.cpp
@@ -586,10 +586,12 @@ bool CompilerStack::analyzeLegacy(bool _noErrorsSoFar)
 		if (source->ast && !typeChecker.checkTypeRequirements(*source->ast))
 			noErrors = false;
 
-	SpillAreaSafetyChecker spillAreaSafetyChecker(m_optimiserSettings, m_errorReporter);
-	for (Source const* source: m_sourceOrder)
-		if (source->ast && !spillAreaSafetyChecker.check(*source->ast))
-			noErrors = false;
+	if (!std::getenv("EVM_DISABLE_MEMORY_SAFE_ASM_CHECK")) {
+		SpillAreaSafetyChecker spillAreaSafetyChecker(m_optimiserSettings, m_errorReporter);
+		for (Source const* source: m_sourceOrder)
+			if (source->ast && !spillAreaSafetyChecker.check(*source->ast))
+				noErrors = false;
+	}
 
 	if (noErrors)
 	{
