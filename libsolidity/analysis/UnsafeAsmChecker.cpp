4c3522b14c54b78dd62a07c5adc0d326b52e91e1
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2025-07-17 15:22:39 +0800

    edit the EVM_DISABLE_MEMORY_SAFE_ASM_CHECK error a little bit

--- libsolidity/analysis/UnsafeAsmChecker.cpp
+++ libsolidity/analysis/UnsafeAsmChecker.cpp
@@ -41,15 +41,15 @@ bool UnsafeAsmChecker::visit(InlineAssembly const& _inlineAsm)
 		m_errorReporter.warning(
 			5726_error,
 			_inlineAsm.location(),
-			"This contract cannot be compiled due to a combination of a memory-unsafe assembly block and a "
+			"This contract cannot be compiled safely due to a combination of a memory-unsafe assembly block and a "
 			"stack-too-deep error. "
 			"The compiler can automatically fix the stack-too-deep error, but only in the absence of memory-unsafe "
 			"assembly.\n"
-			"To successfully compile this contract, please check if this assembly block is memory-safe according to "
+			"To get rid of this error, please check if this assembly block is memory-safe according to "
 			"the requirements at \n\n"
 			"    https://docs.soliditylang.org/en/latest/assembly.html#memory-safety\n\n"
 			"and then mark it with a memory-safe tag.\n"
-			"Alternatively, if you feel confident, you may suppress this error project-wide by "
+			"Alternatively, if you feel confident, you may convert this error into a warning project-wide by "
 			"setting the EVM_DISABLE_MEMORY_SAFE_ASM_CHECK environment variable:\n\n"
 			"    EVM_DISABLE_MEMORY_SAFE_ASM_CHECK=1 <your build command>\n\n"
 			"Please be aware of the memory corruption risks described at the link above!\n");
