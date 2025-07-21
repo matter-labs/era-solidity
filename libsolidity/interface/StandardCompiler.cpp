35ed7b03e5ccb5bb7d92873c775208b97cd35006
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2025-07-21 15:43:04 +0400

    Revert "Temporarily disable yul optimizations (#1053)"
    This reverts commit 5623710eb162dcc1b081739ac515b91f740f4538.

--- libsolidity/interface/StandardCompiler.cpp
+++ libsolidity/interface/StandardCompiler.cpp
@@ -620,9 +620,6 @@ std::variant<OptimiserSettings, Json> parseOptimizerSettings(std::string_view co
 		if (auto jsonError = checkAndSetSpillAreaSize(_jsonInput["spillAreaSize"], settings))
 			return *jsonError;
 
-	settings.runYulOptimiser = false;
-	settings.optimizeStackAllocation = false;
-
 	if (_jsonInput.contains("details"))
 	{
 		Json const& details = _jsonInput["details"];
@@ -642,6 +639,8 @@ std::variant<OptimiserSettings, Json> parseOptimizerSettings(std::string_view co
 		if (auto error = checkOptimizerDetail(details, "cse", settings.runCSE))
 			return *error;
 		if (auto error = checkOptimizerDetail(details, "constantOptimizer", settings.runConstantOptimiser))
+			return *error;
+		if (auto error = checkOptimizerDetail(details, "yul", settings.runYulOptimiser))
 			return *error;
 		if (auto error = checkOptimizerDetail(details, "simpleCounterForLoopUncheckedIncrement", settings.simpleCounterForLoopUncheckedIncrement))
 			return *error;
@@ -954,8 +953,6 @@ std::variant<StandardCompiler::InputsAndSettings, Json> StandardCompiler::parseI
 		ret.optimiserSettings = OptimiserSettings::none();
 	else
 		ret.optimiserSettings = OptimiserSettings::minimal();
-
-	solAssert(!(ret.optimiserSettings.runYulOptimiser || ret.optimiserSettings.optimizeStackAllocation));
 
 	Json const& jsonLibraries = settings.value("libraries", Json::object());
 	if (!jsonLibraries.is_object())
