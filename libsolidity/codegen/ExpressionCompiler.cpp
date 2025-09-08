1130f42453fe529f5afe5b865bc960c72b85a89a
Abinav Puthan Purayil <abinavpp@gmail.com>
2025-09-09 01:49:28 +0200

    Revert "try keeping the old condition"
    This reverts commit d719af72ee178a713749c7e00d8c848f682c990c.

--- libsolidity/codegen/ExpressionCompiler.cpp
+++ libsolidity/codegen/ExpressionCompiler.cpp
@@ -651,10 +651,9 @@ void ExpressionCompiler::generateSelector(FunctionType const& _funcType)
 		FunctionType const* intFuncPtrRefType = intFuncPtrRef->functionType(true);
 		// ContractDefinitionAnnotation::intFuncPtrRefs should only contain refs to internal functions
 		solAssert(intFuncPtrRefType, "");
-		if (!intFuncPtrRefType->hasEqualParameterTypes(_funcType) || !intFuncPtrRefType->hasEqualReturnTypes(_funcType)
-			|| !intFuncPtrRef->isImplemented()
-			|| !(intFuncPtrRefType->parameterTypes().size() == _funcType.parameterTypes().size()
-			  && intFuncPtrRefType->returnParameterTypes().size() == _funcType.returnParameterTypes().size()))
+		if (!(intFuncPtrRefType->parameterTypes().size() == _funcType.parameterTypes().size()
+			  && intFuncPtrRefType->returnParameterTypes().size() == _funcType.returnParameterTypes().size()
+			  && intFuncPtrRef->isImplemented()))
 			continue;
 
 		// The loaded function pointer
